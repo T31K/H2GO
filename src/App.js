f236ca51ebc3df7a19ad9f662138049403e59fd7
T31K <t31kmunwong@gmail.com>
2021-02-01 10:17:17 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-import React, {useState} from 'react'
+import React, {useState, setState} from 'react'
 import { useTimer } from 'react-timer-hook';
 import DarkModeToggle from "react-dark-mode-toggle";
 import logo from "./h2go.svg"
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlayCircle, faPauseCircle } from '@fortawesome/free-solid-svg-icons'
+import { faPlayCircle, faPauseCircle, faPlusCircle, faMinusCircle, faStopCircle} from '@fortawesome/free-solid-svg-icons'
+
+
 
 function MyTimer({ expiryTimestamp }) {
   const {
@@ -20,15 +22,18 @@ function MyTimer({ expiryTimestamp }) {
     resume,
     restart,
   } = useTimer({ expiryTimestamp, onExpire: () => console.log('onExpire called') });
-
   
+
+  const [customTime, setCustomTime] = useState(0)
+
+
   return (
     <div style={{textAlign: 'center'}}>
     
       <h1>H2GO</h1>
       <img src={logo} className="logo"/>
       <div style={{fontSize: '100px'}}>
-       <span>0{minutes}</span>:<span>{seconds}</span>
+       <span> {minutes}</span>:<span>{seconds}</span>
       </div>
       {isRunning ? 
       <div>
@@ -45,13 +50,21 @@ function MyTimer({ expiryTimestamp }) {
         <div className="wave pause-animation"></div>
       </div>
       <FontAwesomeIcon icon={faPlayCircle} onClick={start} className="start"/> 
+      <FontAwesomeIcon icon={faStopCircle} onClick={restart} className="start"/> 
+      <div className="buttons">
+        <FontAwesomeIcon icon={faPlusCircle} onClick={(e)=>setCustomTime(customTime+1)} className="plus"/> 
+        <FontAwesomeIcon icon={faMinusCircle} onClick={(e)=>setCustomTime(customTime-1)} className="minus"/> 
+      </div>
+      <input type="number" onChange={(e)=>setCustomTime(e.target.value)}></input>
       </div>
       }
 
       <button onClick={() => {
         // Restarts to 5 minutes timer
+        console.log(customTime)
+        let new_time = customTime * 60
         const time = new Date();
-        time.setSeconds(time.getSeconds() + 300);
+        time.setSeconds(time.getSeconds() + new_time);
         restart(time)
       }}>Restart</button>
     </div>
@@ -61,7 +74,7 @@ function MyTimer({ expiryTimestamp }) {
 export default function App() {
 
   const time = new Date();
-  time.setSeconds(time.getSeconds() + 10); // 10 minutes timer
+  time.setSeconds(time.getSeconds() + 500); // 10 minutes timer
   return (
     <div>
    
